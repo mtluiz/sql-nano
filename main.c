c588b609ec6910983685a7d430d28ce460f1dff7
Matheus Madureira <matheusluizn@gmail.com>
2022-05-07 18:04:27 -0300

    feat: planning persistance

--- main.c
+++ main.c
@@ -75,8 +75,16 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
+typedef struct Pager
+{
+    int file_descriptor;
+    uint32_t file_lenght;
+    void* pages[TABLE_MAX_PAGES];
+};
+
 typedef struct
 {
+    void* pages[TABLE_MAX_PAGES];
     uint32_t num_rows;
     void *pages[TABLE_MAX_PAGES];
 } Table;
